03b9929946db5a583bb752a7495e0f9bcb059d95
David Holt <djholty@gmail.com>
2025-10-19 13:41:21 -0400

    Update rebuild_database.py path to data/ingested_documents

--- rebuild_database.py
+++ rebuild_database.py
@@ -17,7 +17,7 @@ load_dotenv()
 os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
 
 # Paths
-MARKDOWN_DIR = "./ingested_documents"
+MARKDOWN_DIR = "./data/ingested_documents"
 DB_DIR = "./data/chroma_db"
 OLD_DB_DIR = "./data/optimized_2000_markdown_chroma_db"
 
